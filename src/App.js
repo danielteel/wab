174c09f7aa1764caeddc5ff10a2a61f709e28e90
Daniel Teel <daniel.teel.2@us.af.mil>
2021-10-08 13:21:42 -0600

    Working on useAppState hook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
 import NumberPad from './components/NumberPad';
 import React, { useEffect, useRef } from 'react';
 
-
-function loadAppStateString(){
-    let stateString;
-    try {
-        stateString = localStorage.getItem("state");
-        if (JSON.parse(stateString).magic!==7331) throw Error("State aint valid");
-        console.log("Loaded state from localStorage",stateString);
-    } catch {
-        stateString=JSON.stringify({
-            magic: 7331,
-            standardKit: [],
-            standardCargo: [],
-            aircraft: [],
-            forms: []
-        });
-        console.log("Loaded default state", stateString);
-    }
-    return stateString;
-}
+const saveAppStateTimeout = 2000;//How long it should wait for any other state changes to save the state to localStorage
 
 function saveAppStateString(state){
     localStorage.setItem("state", state);
     console.log("Saved to localStorage", state)
 }
 
+function loadAppStateString(){
+    let stateString;
+    let needsSavedToStorage=false;
+    try {
+        stateString = localStorage.getItem("state");
+        const stateObj = JSON.parse(stateString);
+        const fixField = (field, newData) => {
+            console.log("Fixing "+field+" since it aint right");
+            stateObj[field]=newData;
+            needsSavedToStorage=true;
+        }
+        if (!Array.isArray(stateObj.standardKit)) fixField('standardKit',[]);
+        if (!Array.isArray(stateObj.standardCargo)) fixField('standardCargo',[]);
+        if (!Array.isArray(stateObj.aircraft)) fixField('aircraft',[]);
+        if (!Array.isArray(stateObj.forms)) fixField('forms',[]);
+        
+        if (needsSavedToStorage) stateString=JSON.stringify(stateObj);
+
+        console.log("Loaded state from localStorage",stateString);
+    } catch {
+        stateString=JSON.stringify({
+            standardKit: [],
+            standardCargo: [],
+            aircraft: [],
+            forms: []
+        });
+        needsSavedToStorage=true;
+        console.log("Loaded default state", stateString);
+    }
+    if (needsSavedToStorage) saveAppStateString(stateString);
+    return stateString;
+}
+
+
 function App() {
     const [appStateString, setAppStateString] = React.useState(()=>loadAppStateString());
     const appState = JSON.parse(appStateString);
 
     const firstRender = useRef(true);
-
     useEffect(()=>{
         if (firstRender.current) {
             firstRender.current = false;
             return;
         }
-        const timeoutId = setTimeout(()=>saveAppStateString(JSON.stringify(appState)), 5000);
+        let timeoutId = setTimeout(()=>{
+            console.log("Save timeout id",timeoutId, "fired");
+            saveAppStateString(JSON.stringify(appState));
+            timeoutId=null;
+        }, saveAppStateTimeout);
+        console.log("Setting save timeout id",timeoutId, saveAppStateTimeout,"ms");
         return () => {
-            console.log("Clearing timeout");
-            clearTimeout(timeoutId);
+            if (timeoutId){
+                console.log("Clearing save timeout id",timeoutId);
+                clearTimeout(timeoutId);
+            }
         }
     }, [appState]);
 
