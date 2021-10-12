dab09e93f104e05fcdd85d45d30a2cbf2b9fb9e8
Daniel Teel <daniel.teel.2@us.af.mil>
2021-10-11 19:47:41 -0600

    trying to figure out a good scheme for useLocalStorageArray

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import NumberPad from './components/NumberPad';
 import React, { useEffect, useRef, useState } from 'react';
 
-import {useLocalStorageArray} from './useLocalStorage';
+import {useLocalStorageArray, useLocalStorageArrayItem} from './useLocalStorage';
 
+
+function ViewItem({item, closeView, deleteItem}){
+    const [itemState, setItemState] = useLocalStorageArrayItem(item);
+    console.log(item, itemState);
+    return <div>
+        <input type="text" value={itemState.title} onChange={(e)=>setItemState({title:e.target.value})}/>
+        <button onClick={()=>deleteItem(item)}>Delete Item</button>
+        <button onClick={closeView}>Close Item</button>
+    </div>
+}
 
 function App({nameSpace}) {
-    const [keys, addItem, deleteItem] = useLocalStorageArray(nameSpace, 'number-array');
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [keys, addItem, deleteItem, getItem, setItem] = useLocalStorageArray(nameSpace, 'number-array');
     console.log("Keys",typeof keys);
     return (
         <div className="App" style={{backgroundColor: '#AAAAAA'}}><br/>
             <button onClick={()=>{
-                addItem(0);
+                addItem({title: 'Unnamed'});
             }}>New item</button>
             {
+                selectedItem
+                ?
+                    <ViewItem key={selectedItem} item={selectedItem} closeView={()=>setSelectedItem(null)} deleteItem={(item)=>{setSelectedItem(null);deleteItem(item);}}/>
+                :
+                    null
+            }
+            {
                 keys.map( key => <button key={key} onClick={()=>{
-                        deleteItem(key);
+                    setSelectedItem(key);
                 }}>{key}</button>)
             }
         </div>
--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -28,16 +28,27 @@ function isKeyAMatch(key, nameSpaceToCheck, arrayNameToCheck, indexToCheck=null)
     return false;
 }
 
-//Return all current keys in the 'array' and a free key name for a new one
-function getArrayKeys(nameSpaceItsIn, arrayName){
+
+//Returns a key that is free according to the key list, nameSpace, and arrayName
+function freeKeyFromList(keyList, nameSpace, arrayName){
+    let freeIndex=0;
+    for (const key of keyList){
+        const [isValid, , , curIndex] = deconstructArrayKey(key);
+        if (isValid){
+            if (curIndex>=freeIndex) freeIndex=curIndex+1;
+        }
+    }
+    return constructArrayKey(nameSpace, arrayName, freeIndex);
+}
+
+//Return all current keys in localStorage in the 'array' and a free key name for a new one
+function readArrayKeys(nameSpaceItsIn, arrayName){
     let keys=[];
-    let freeIndex = 0;
     for (var i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
         const [isValid, curNameSpace, curArrayName, curIndex] = deconstructArrayKey(key);
         if (isValid){
             if (nameSpaceItsIn===curNameSpace && arrayName===curArrayName){
-                if (curIndex>=freeIndex) freeIndex=curIndex+1;
                 keys.push({key: key, index: curIndex});
             }
         }
@@ -45,8 +56,7 @@ function getArrayKeys(nameSpaceItsIn, arrayName){
     keys.sort( (a,b) => a.index-b.index);
     keys=keys.map( key => key.key );
 
-    const freeKey=constructArrayKey(nameSpaceItsIn, arrayName, freeIndex);
-    return [keys, freeKey];
+    return keys;
 }
 
 function saveToStorage(key, value){
@@ -54,118 +64,157 @@ function saveToStorage(key, value){
     try {
         localStorage.setItem(key, JSON.stringify(value));
     } catch {
-        console.error("Cant save to localStorage, maybe in private session?")
+        console.error("saveToStorage: cant save to localStorage key:", key," value:",value)
     }
 }
 
+
+let localStorageEventListeners = [];
+
+function localStorageAddListener(nameSpace, arrayName, eventCallback){
+    localStorageEventListeners.push({nameSpace, arrayName, eventCallback});
+}
+
+function localStorageRemoveListener(nameSpace, arrayName, eventCallback){
+    localStorageEventListeners=localStorageEventListeners.filter(i => !(i.nameSpace===nameSpace && i.arrayName===arrayName && i.eventCallback===eventCallback) );
+}
+
+function localStorageEvent(nameSpace, arrayName, action){
+
+}
+
+
+
 function useLocalStorageArray(nameSpace, arrayName){
-    const [[keysInArray, freeKey], setKeys] = useState( () => getArrayKeys(nameSpace,arrayName) );
+    const [keys, setKeys] = useState( () => readArrayKeys(nameSpace,arrayName) );
 
-    const addItem = (value) => {
-        setKeys( ([oldKeysInArray, oldFreeKey])=>{
-            saveToStorage(oldFreeKey, value);
+    //Add item function, call this with a value to create a new item with value. If you want to know what this new key is, pass in a callback function that will get called with the key
+    const addItem = (value, callbackWithNewKey) => {
+        setKeys( (oldKeys) => {
+            const freeKey = freeKeyFromList(oldKeys, nameSpace, arrayName);
 
-            let [isValid, , , freeIndex] = deconstructArrayKey(oldFreeKey);
-            if (!isValid){
-                console.error("Failed to save key",oldFreeKey);
-                return [oldKeysInArray, oldFreeKey];
-            }
-         return [[...oldKeysInArray, oldFreeKey ], constructArrayKey(nameSpace, arrayName, ++freeIndex)];
-        })
+            saveToStorage(freeKey, value);
+
+            if (typeof callbackWithNewKey==='function') callbackWithNewKey(freeKey);
+
+            return [...oldKeys, freeKey];
+        });
     }
 
-    const deleteItem = (key) => {
-        setKeys( ([oldKeysInArray, oldFreeKey]) => {
-            localStorage.removeItem(key);
-            return [oldKeysInArray.filter( currentKey => currentKey !== key ), oldFreeKey];
-        })
+    //Delete item function, call this with the key name and it'll remove it from the list if it makes sense to
+    const deleteItem = (key, _internal_alreadyRemoved=false) => {
+        if (isKeyAMatch(key, nameSpace, arrayName)){
+            setKeys( (oldKeys) => {
+                if (_internal_alreadyRemoved===false) localStorage.removeItem(key);
+                return oldKeys.filter( currentKey => currentKey !== key );
+            });
+        }
     }
 
+    const getItem = (key) => {
+
+    }
+
+    const setItem = (key, value) => {
+
+    }
+
+    //Listen for localStorage changes and update our list
     useEffect(() => {
         const handler = (e) => {
             if (e.storageArea!==localStorage) return;
-            if (e.key===null){
-                setKeys( ([oldKeysInArray, oldFreeKey]) => getArrayKeys(nameSpace,arrayName) )//All data has been cleared
-            } else if (e.newValue===null && keysInArray.includes(e.key)){
-                deleteItem()//Singular key has been deleted
+
+            if (e.key===null){//All keys have been cleared
+                setKeys( () => readArrayKeys(nameSpace,arrayName) );
+
+            } else if (e.newValue===null && keys.includes(e.key)){//key has been deleted
+                deleteItem(e.key, true);
+                
             } else if (e.key){
-                if (isKeyAMatch(e.key, nameSpace, arrayName)){
-                    
+                if (isKeyAMatch(e.key, nameSpace, arrayName)){//Key added or changed
+                    if (!keys.includes(e.key)){
+                        setKeys( (oldKeys) => {
+                            const newKeyList = [...oldKeys, e.key];
+                            return newKeyList;
+                        });
+                    }
                 }
             }
         };
+
         window.addEventListener('storage', handler);
         return () => {
             window.removeEventListener('storage', handler);
         };
     });
-    
-    return [keysInArray, addItem, deleteItem];
+
+    return [keys, addItem, deleteItem, getItem, setItem];
 }
 
-export {useLocalStorageArray};
+
+function useLocalStorageArrayItem(key, defaultValue, saveHysteresis=1000){
+    //useState and initiliaze to either whats stored in localStorage or the defaultValue passed in
+    const [state, setState] = useState( () => {
+        let storedObj;
+        let storedString=localStorage.getItem(key);
+        if (storedString!==null){
+            //Key exists, try and parse it as a JSON object
+            try {
+                storedObj = JSON.parse(storedString);
+            } catch {
+                //failed to parse it, leave storedObj undefined so we know to initiliaze
+            }
+        }
+        if (!storedObj){
+            //Key doesnt exist or its not a valid JSON object, initiliaze to defaultValue and try to save this key
+            storedObj=defaultValue;
+            try {
+                localStorage.setItem(key, defaultValue);
+            } catch {
+                console.error("useLocalStorageArrayItem: failed to save key to localStorage key:",key);
+            }
+        }
+        return storedObj;
+    });
 
 
+    const firstRender = useRef({firstRender: true, key: key});
+    useEffect(()=>{
+        if (firstRender.current) {//No need to set a timeout and save if its just the initial render.
+            firstRender.current = false;
+            return;
+        }
+        let timeoutId = setTimeout(()=>{
+            saveToStorage(key, state);
+            timeoutId=null;
+        }, saveHysteresis);
+        return () => {
+            if (timeoutId){
+                clearTimeout(timeoutId);
+            }
+        }
+    }, [state, key, saveHysteresis]);
 
 
+    //Check if key is valid, if its not return null and a function that does nothing
+    const [isValid] = deconstructArrayKey(key);
+    if (!isValid) return [null,()=>{}];
 
-// function getFromStorage(key, defaultValue){
-//     let stateObj;
-//     try {
-//         stateObj = JSON.parse(localStorage.getItem(key));
-//         if (!stateObj) throw Error();
-//     } catch {
-//         stateObj = defaultValue;
-//         saveToStorage(key, defaultValue);
-//     }
-//     return stateObj;
-// }
+    //This is the function users will call to set the state, which will also save to localStorage after a timeout period.
+    //It makes shallow copies to force a re-render which will update localStorage
+    const setStateCallback = (newValue) => {
+        if (typeof newValue==='function'){
+            setState( (oldValue) => newValue(oldValue) );
+        }else if (Array.isArray(newValue)){
+            setState( () => [...newValue] );
+        }else if (typeof newValue==='object' && newValue!==null){
+            setState( () => ({...newValue}) );
+        }else{
+            setState( () => newValue );
+        }
+    }
 
+    return [state, setStateCallback];
+}
 
-// function useLocalStorageState(nameSpace, key, index, defaultValue, saveHysteresis=500){
-//     if (nameSpace.includes('$') || key.includes('$')) throw Error("nameSpace and key should not include any '$'s");
-//     const localStorageKey = buildKey(nameSpace,key,index);
-
-//     const [state, setState] = useState( () => getFromStorage(localStorageKey, defaultValue) );
-
-//     const firstRender = useRef(true);
-//     useEffect(()=>{
-//         if (firstRender.current) {//No need to set a timeout and save if its just the initial render.
-//             firstRender.current = false;
-//             return;
-//         }
-//         let timeoutId = setTimeout(()=>{
-//             saveToStorage(localStorageKey, state);
-//             timeoutId=null;
-//         }, saveHysteresis);
-//         return () => {
-//             if (timeoutId){
-//                 clearTimeout(timeoutId);
-//             }
-//         }
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, [state, localStorageKey]);
-
-//     let returnedState;
-//     if (Array.isArray(state)){
-//         returnedState = [...state];
-//     }else if (typeof state==='object'){
-//         returnedState = {...state};
-//     }else{
-//         returnedState = state;
-//     }
-
-//     return [returnedState, (newAppState)=>{
-//         if (typeof newAppState==='function'){
-//             if (Array.isArray(state)){
-//                 setState( (v) => newAppState([...v]) );
-//             }else if (typeof state==='object'){
-//                 setState( (v) => newAppState({...v}) );
-//             }else{
-//                 setState( (v) => newAppState(v) );
-//             }
-//         }else{
-//             setState( newAppState );
-//         }
-//     }];
-// }
+export {useLocalStorageArray, useLocalStorageArrayItem};
